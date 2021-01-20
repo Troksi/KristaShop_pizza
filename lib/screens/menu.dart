c086b726fcfbb3abc1591e4073fd6e875aaf9dc3
FlutLab.io <>
2021-01-20 23:19:19 +0000

    Autosave

---
+++ lib/screens/menu.dart
@@ -0,0 +1,15 @@
+class Menu extends StatefulWidget {
+    Menu(({Key key}):super(key:key));
+
+  @override
+  _MenuState createState() => _MenuState();
+}
+
+class _MenuState extends State<Menu> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child:child,
+    );
+  }
+}
