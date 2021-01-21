2c59bb79d993ddd39cc2978aa9d85eb15ebe928c
FlutLab.io <>
2021-01-21 03:48:35 +0000

    Autosave

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -14,33 +14,28 @@ class _MenuState extends State<Menu> {
           appBar: AppBar(
             title: Text('Меню'),
           ),
-          body: GridView.builder(
-                    padding: const EdgeInsets.all(10.0),
-                    itemCount: 4,
-                    itemBuilder: (BuildContext context, int index){
-                        return Card(
-                            child: ListTile(
-                                leading: Container(
-                                    width:36,
-                                    height:36,
-                                    child: Placeholder(),
-                                ),//Container
-                                title: Text("Название товара" + index.toString()),
-                                subtitle: Text("Краткое описание"),
-                                trailing: IconButton(
-                                                    icon: Icon(Icons.add_shopping_cart),
-                                                    onPressed: () {},
-                                                ),//IconButton
-                            ),//ListTitle
-                        );//Card
-                    },
-                    //gridDelegate: SliverGridDelegateWithCrossAxisCount(
-                        //crossAxisCount: 2,
-                       // childAspectRatio: 3/2,
-                      //  crossAxisSpacing: 10,
-                    //    mainAxisSpacing: 10,
-                   // ),
-                )
+          body:  FutureBuilder(
+            future: DataDumper.getProductsOnline(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.hasError) {
+                    _onWidgetDidBuild(() {
+                    Scaffold.of(context).showSnackBar(SnackBar(
+                        content: Text('${snapshot.error}'),
+                        backgroundColor: Colors.red,
+                    ));
+                    });
+                }
+                if (snapshot.hasData) {
+                    _products = snapshot.data;
+                    return makeList();
+                }
+                else {
+                    return Center(
+                        child: CircularProgressIndicator(), 
+                    );
+                }
+            }, 
+        ),
         );
   }
 }
