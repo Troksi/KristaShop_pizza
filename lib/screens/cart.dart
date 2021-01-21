2020d6b4560d13afdb66011d8edc3c2a04a64644
FlutLab.io <>
2021-01-21 08:33:59 +0000

    Autosave

---
+++ lib/screens/cart.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:pizza/screens/data.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:pizza/screens/product.dart';
+import 'package:pizza/models/productModel.dart';
+
+class Cart extends StatefulWidget {
+  Cart({Key key}) : super(key: key);
+
+  @override
+  _CartState createState() => _CartState();
+}
+
+class _CartState extends State<Cart> {
+  List<ProductModel> _products = List<ProductModel>();
+
+  @override
+  void initState() {
+    _products = DataPreLoader.getCart();
+    super.initState();
+  }
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text("Корзина"),
+        ),
+        body: 
+        _products.length != 0 ?
+            Column(
+            children: [
+                Flexible(
+                    child: ListView.builder(
+                        itemCount: _products.length,
+                        itemBuilder: (BuildContext context, int index) {
+                            return Card(
+                                child: ListTile(
+                                    leading: Container(
+                                        width: 36,
+                                        height: 36,                            
+                                        child: Image.network(
+                                            _products.elementAt(index).imgUrl,
+                                            fit: BoxFit.cover,
+                                        ),
+                                    ),
+                                    title: Text(_products.elementAt(index).title),
+                                    subtitle: Text(_products.elementAt(index).subtitle),
+                                    trailing: IconButton(
+                                        icon: Icon(Icons.remove_shopping_cart),
+                                        tooltip: 'Удалить товар из корзины',
+                                        onPressed: () {
+                                            setState(() {
+                                                DataPreLoader.removeCart(_products.elementAt(index));
+                                            });                                
+                                        },
+                                    ),
+                                    onTap: () {
+                                        Navigator.push(
+                                            context, 
+                                            CupertinoPageRoute(builder: (_) => 
+                                            Product(model: _products.elementAt(index))));
+                                    },
+                                ),
+                                
+                            );
+                        },
+                    ),
+                ),
+                RaisedButton(
+                    onPressed: () {}, 
+                    padding: const EdgeInsets.all(0.0),
+                    color: Colors.green,
+                    child: Container(              
+                        padding: const EdgeInsets.all(10.0),
+                        child:
+                            Text(
+                                "Оформить заказ на сумму: " + DataPreLoader.getCartPrice().toString() + " ₽", 
+                                style: TextStyle(fontSize: 20),
+                            ),
+                    ),
+                ),
+                
+            ],                
+            )            
+            : Center(
+                child: Text("Корзина пуста"),
+            ),
+                               
+            
+        );
+  }
+}
---
+++ lib/screens/profile.dart
@@ -0,0 +1,184 @@
+pizzaimport 'package:flutter/material.dart';
+
+class Profile extends StatefulWidget {
+  Profile({Key key}) : super(key: key);
+
+  @override
+  _ProfileState createState() => _ProfileState();
+  
+}
+
+class _ProfileState extends State<Profile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text("Профиль"),
+        ),
+        body: Padding(
+            padding: const EdgeInsets.all(15),
+            child: Column(
+                children: [
+                    CircleAvatar(
+                        radius: 40,
+                        backgroundImage: NetworkImage(
+                            'https://i.gifer.com/ZbS5.gif'),
+                    ),
+                    Text('\n'),
+                    Table(
+                    border: TableBorder.all(color: Colors.green),
+                        children: [
+                            TableRow(children: [
+                            ListTile(                    
+                                title: 
+                                    Text(
+                                        "Ваш ID",
+                                        textAlign: TextAlign.center, 
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold, 
+                                            fontSize: 12,
+                                            color: Colors.black                                                                            
+                                        ),
+                                    ),
+                                subtitle: 
+                                    Text(
+                                        "23543259", 
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                            fontSize: 12,
+                                            color: Colors.green                                
+                                        ),
+                                    ),
+                            ),
+                            ListTile(                    
+                                title: 
+                                    Text(
+                                        "Статус",
+                                        textAlign: TextAlign.center, 
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold, 
+                                            fontSize: 12,
+                                            color: Colors.black                                
+                                        ),                                
+                                    ),
+                                subtitle: 
+                                    Text(
+                                        "Постоянный клиент", 
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                            fontSize: 12,
+                                            color: Colors.green                                
+                                        ),
+                                    ),
+                            ),               
+                            ]),
+                        ],
+                    ),
+
+                    ListTile(                    
+                        title: 
+                            Text(
+                                "Эл. почта", 
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, 
+                                    fontSize: 16,
+                                    color: Colors.black),                                
+                            ),
+                        subtitle: 
+                            Text(
+                                "ars_pro@bk.ru", 
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.green),
+                            ),
+                        onTap: () {},
+                    ),
+                    const Divider(
+                        color: Colors.green,
+                        height: 15,
+                        thickness: 1,
+                        indent: 15,
+                        endIndent: 15,
+                    ),
+
+                    ListTile(                    
+                        title: 
+                            Text(
+                                "Ваше имя", 
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, 
+                                    fontSize: 16,
+                                    color: Colors.black),
+                            ),
+                        subtitle: 
+                            Text(
+                                "Евгений",
+                                style: TextStyle(
+                                    fontSize: 16, 
+                                    color: Colors.green),
+                            ),
+                        onTap: () {},
+                    ),
+                    const Divider(
+                        color: Colors.green,
+                        height: 15,
+                        thickness: 1,
+                        indent: 15,
+                        endIndent: 15,
+                    ),
+
+                    ListTile(                    
+                        title: 
+                            Text(
+                                "Телефон номер", 
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, 
+                                    fontSize: 16,
+                                    color: Colors.black),
+                            ),
+                        subtitle: 
+                            Text(
+                                "+7 (980) 749 75 72",
+                                style: TextStyle(
+                                    fontSize: 16, 
+                                    color: Colors.green), 
+                            ),
+                        onTap: () {},
+                    ),
+                    const Divider(
+                        color: Colors.green,
+                        height: 15,
+                        thickness: 1,
+                        indent: 15,
+                        endIndent: 15,
+                    ),
+                    ListTile(                    
+                        title: 
+                            Text(
+                                "Дата рождения", 
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, 
+                                    fontSize: 16,
+                                    color: Colors.black),
+                            ),
+                        subtitle: 
+                            Text(
+                                "18.9.1998",
+                                style: TextStyle(
+                                    fontSize: 16, 
+                                    color: Colors.green), 
+                            ),
+                        onTap: () {},
+                    ),
+
+                          
+                ],
+            ),
+            
+                
+        ),
+        ,
+        
+    );
+  }
+}
