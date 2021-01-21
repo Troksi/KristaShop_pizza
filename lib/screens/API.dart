375366320d9d73debc9fc3cdbdb71af39292ee8b
FlutLab.io <>
2021-01-21 08:15:56 +0000

    Autosave

--- lib/screens/API.dart
+++ lib/screens/API.dart
@@ -3,8 +3,8 @@ import 'dart:async';
 
 class API {
     static Future<http.Response> getProducts() async {
-        var result = 
-            await http.get("https://6008facd0a54690017fc27e7.mockapi.io/products");
+        var result =
+        await http.get("https://6008facd0a54690017fc27e7.mockapi.io/products");
         
         if (result.statusCode == 200) {
             return result;
